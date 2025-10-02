64e359f1ca75cf02eca056014faf8c868d3a092f
Chris McCormick <chris@mccormick.cx>
2025-10-02 08:38:57 +0800

    Analyze build size with npm run analyze.

--- src/demo/vite.config.js
+++ src/demo/vite.config.js
@@ -1,9 +1,10 @@
 import { env } from "process"
 import { defineConfig } from "vite"
 import { viteSingleFile } from "vite-plugin-singlefile"
+import { analyzer } from 'vite-bundle-analyzer'
 
 export default defineConfig({
-  plugins: [viteSingleFile({ removeViteModuleLoader: true })],
+  plugins: [env["ANALYZE"] && analyzer(), viteSingleFile({ removeViteModuleLoader: true })],
   build: {
     // minify: "terser", // slightly smaller
     outDir: "../../docs",
