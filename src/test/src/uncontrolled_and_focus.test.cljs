382234cd9d981942557a38a02577f7e0dec7dd11
Chris McCormick <chris@mccormick.cx>
2025-10-18 15:57:32 +0800

    More tests for unkeyed input position change.

--- src/test/src/uncontrolled_and_focus.test.cljs
+++ src/test/src/uncontrolled_and_focus.test.cljs
@@ -37,29 +37,117 @@
             ;; The user's change should be preserved
             (th/assert-equal (.-checked checkbox) true))))))) 
 
+(defn- focus-input [container]
+  (let [input (.querySelector container "#focusable")]
+    (set! (.-value input) "a word")
+    (.focus input)
+    (.setSelectionRange input 2 5)
+    (th/assert-equal js/document.activeElement input)
+    input))
+
+(defn- validate-focus [input message]
+  (th/assert-equal js/document.activeElement input message)
+  (th/assert-equal (.-selectionStart input) 2)
+  (th/assert-equal (.-selectionEnd input) 5))
+
+(def dynamic-list-state (r/atom {:before [] :after []}))
+
+(defn dynamic-list [{:keys [unkeyed? as]
+                     :or {as :p}}]
+  [:div
+   (for [v (:before @dynamic-list-state)]
+     (with-meta (if (= as :input) [:input {:type "text"}] [as v])
+                (when-not unkeyed? {:key v})))
+   [:input {:id "focusable"}]
+   (for [v (:after @dynamic-list-state)]
+     (with-meta (if (= as :input) [:input {:type "text"}] [as v])
+                (when-not unkeyed? {:key v})))])
+
+(defn conditional-list [{:keys [show-0? show-1? show-2? show-3?]}]
+  [:div
+   (when show-0? [:p 0])
+   (when show-1? [:p 1])
+   [:input {:id "focusable"}]
+   (when show-2? [:p 2])
+   (when show-3? [:p 3])])
+
+(def fragment-focus-state (r/atom {:active? false}))
+
+(defn fragment-focus-app []
+  (let [input-ref (atom nil)]
+    (fn []
+      [:div
+       [:h1 "Heading"]
+       (if-not (:active? @fragment-focus-state)
+         [:<>
+          "foobar"
+          [:<>
+           "Hello World"
+           [:h2 "yo"]]
+          [:input {:type "text" :id "focusable" :ref #(reset! input-ref %)}]]
+         [:<>
+          [:<>
+           "Hello World"
+           [:h2 "yo"]]
+          "foobar"
+          [:input {:type "text" :id "focusable" :ref #(reset! input-ref %)}]])])))
+
+(def selection-state (r/atom {:active? false}))
+
+(defn selection-app []
+  (let [input-ref (atom nil)]
+    (fn []
+      [:div
+       [:h1 "Heading"]
+       (if-not (:active? @selection-state)
+         [:<>
+          "foobar"
+          [:<>
+           "Hello World"
+           [:h2 "yo"]]
+          [:input {:type "text" :id "focusable" :value "foobar" :ref #(reset! input-ref %)}]]
+         [:<>
+          [:<>
+           "Hello World"
+           [:h2 "yo"]]
+          "foobar"
+          [:input {:type "text" :id "focusable" :value "foobar" :ref #(reset! input-ref %)}]])])))
+
+(defn focus-app [{:keys [show-first? show-last?]}]
+  [:div
+   (when show-first? [:p "first"])
+   [:input {:id "focusable"}]
+   (when show-last? [:p "last"])])
+
 (describe "Focus and Selection Management"
   (fn []
     (it "should maintain focus when moving an input"
       (fn []
-        (let [order (r/atom ["a" "b" "c"])
-              component (fn []
-                          [:div
-                           (for [item @order]
-                             (if (= item "b")
-                               ^{:key "b"} [:input {:id "focusable"}]
-                               ^{:key item} [:p item]))])
-              container (.createElement js/document "div")]
+        (let [container (.createElement js/document "div")]
           (.appendChild js/document.body container)
-          (r/render [component] container)
 
-          (let [input (.querySelector container "#focusable")]
-            (.focus input)
-            (th/assert-equal js/document.activeElement input)
+          ;; 1. Initial render: middle
+          (r/render [focus-app {:show-first? true :show-last? true}] container)
 
-            ;; Reorder the elements, moving the input
-            (reset! order ["b" "a" "c"])
+          ;; 2. Move to beginning
+          (let [input (focus-input container)]
+            (r/render [focus-app {:show-first? false :show-last? true}] container)
+            (validate-focus input "move from middle to beginning"))
 
-            (th/assert-equal js/document.activeElement input "Focus should be maintained after reorder")))))
+          ;; 3. Move to middle
+          (let [input (focus-input container)]
+            (r/render [focus-app {:show-first? true :show-last? true}] container)
+            (validate-focus input "move from beginning to middle"))
+
+          ;; 4. Move to end
+          (let [input (focus-input container)]
+            (r/render [focus-app {:show-first? true :show-last? false}] container)
+            (validate-focus input "move from middle to end"))
+
+          ;; 5. Move to middle again
+          (let [input (focus-input container)]
+            (r/render [focus-app {:show-first? true :show-last? true}] container)
+            (validate-focus input "move from end to middle")))))
 
     (it "should keep text selection on re-render"
       (fn []
@@ -83,4 +171,136 @@
             (swap! state assoc :text "hello world")
 
             (th/assert-equal (.-selectionStart input) 1 "selectionStart should be preserved")
-            (th/assert-equal (.-selectionEnd input) 4 "selectionEnd should be preserved")))))))
+            (th/assert-equal (.-selectionEnd input) 4 "selectionEnd should be preserved")))))
+
+    (it "should maintain focus when adding children around input"
+      (fn []
+        (reset! dynamic-list-state {:before [] :after []})
+        (let [container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+          (r/render [dynamic-list] container)
+          (let [input (focus-input container)
+                prepend #(swap! dynamic-list-state update :before (fn [b] (cons (th/rand) b)))
+                append #(swap! dynamic-list-state update :after (fn [a] (conj a (th/rand))))]
+
+            (prepend)
+            (validate-focus input "insert sibling before")
+
+            (append)
+            (validate-focus input "insert sibling after")
+
+            (append)
+            (validate-focus input "insert sibling after again")
+
+            (prepend)
+            (validate-focus input "insert sibling before again")))))
+
+    (it "should maintain focus when adding children around input (unkeyed)"
+      (fn []
+        (reset! dynamic-list-state {:before [] :after []})
+        (let [container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+          (r/render [dynamic-list {:unkeyed? true}] container)
+          (let [input (focus-input container)
+                prepend #(swap! dynamic-list-state update :before (fn [b] (cons (th/rand) b)))
+                append #(swap! dynamic-list-state update :after (fn [a] (conj a (th/rand))))]
+
+            (prepend)
+            (validate-focus input "insert sibling before")
+
+            (append)
+            (validate-focus input "insert sibling after")
+
+            (append)
+            (validate-focus input "insert sibling after again")
+
+            (prepend)
+            (validate-focus input "insert sibling before again")))))
+
+    (it "should maintain focus when conditional elements around input"
+      (fn []
+        (let [container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+          (r/render [conditional-list {:show-0? true :show-1? true :show-2? true :show-3? true}] container)
+          (let [input (focus-input container)]
+            (r/render [conditional-list {:show-1? true :show-2? true :show-3? true}] container)
+            (validate-focus input "remove sibling before")
+
+            (r/render [conditional-list {:show-1? true :show-2? true}] container)
+            (validate-focus input "remove sibling after")
+
+            (r/render [conditional-list {:show-1? true}] container)
+            (validate-focus input "remove sibling after 2")
+
+            (r/render [conditional-list {}] container)
+            (validate-focus input "remove sibling before 2")))))
+
+    (it "should maintain focus when removing elements around input"
+      (fn []
+        (reset! dynamic-list-state {:before [0 1] :after [2 3]})
+        (let [container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+          (r/render [dynamic-list] container)
+          (let [input (focus-input container)
+                shift #(swap! dynamic-list-state update :before rest)
+                pop #(swap! dynamic-list-state update :after butlast)]
+
+            (shift)
+            (validate-focus input "remove sibling before")
+
+            (pop)
+            (validate-focus input "remove sibling after")
+
+            (pop)
+            (validate-focus input "remove sibling after 2")
+
+            (shift)
+            (validate-focus input "remove sibling before 2")))))
+
+    (it "should maintain focus when adding input next to the current input"
+      (fn []
+        (reset! dynamic-list-state {:before [] :after []})
+        (let [container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+          (r/render [dynamic-list {:as :input}] container)
+          (let [input (focus-input container)
+                prepend #(swap! dynamic-list-state update :before (fn [b] (cons (th/rand) b)))
+                append #(swap! dynamic-list-state update :after (fn [a] (conj a (th/rand))))]
+
+            (prepend)
+            (validate-focus input "add input before")
+
+            (append)
+            (validate-focus input "add input after")
+
+            (prepend)
+            (validate-focus input "add input before again")))))
+
+    ;;; Eucalypt does not support hydration, so the Preact test 'should maintain focus when hydrating' is not applicable.
+
+    (it "should keep focus in Fragments"
+      (fn []
+        (reset! fragment-focus-state {:active? false})
+        (let [container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+          (r/render [fragment-focus-app] container)
+          (let [input (.querySelector container "#focusable")]
+            (.focus input)
+            (swap! fragment-focus-state assoc :active? true)
+            (th/assert-equal js/document.activeElement input "After rerender")))))
+
+    (it "should keep text selection when reordering fragments"
+      (fn []
+        (reset! selection-state {:active? false})
+        (let [container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+          (r/render [selection-app] container)
+          (let [input (.querySelector container "#focusable")]
+            (.focus input)
+            (.setSelectionRange input 2 5)
+            (swap! selection-state assoc :active? true)
+
+            (th/assert-equal js/document.activeElement input "Before rerender")
+            (th/assert-equal (.-selectionStart input) 2)
+            (th/assert-equal (.-selectionEnd input) 5)
+            (th/assert-equal js/document.activeElement input "After rerender")))))))
