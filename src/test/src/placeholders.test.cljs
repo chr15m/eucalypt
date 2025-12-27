4fef7f113a8bebe7d21cfa4a1c5a26531930d12f
Chris McCormick <chris@mccormick.cx>
2025-12-27 21:29:20 +0800

    More placeholder tests implemented.

--- src/test/src/placeholders.test.cljs
+++ src/test/src/placeholders.test.cljs
@@ -16,6 +16,17 @@
      [:div "Hello"]
      sibling]))
 
+(defn create-nullable [name]
+  (fn [{:keys [show?]}]
+    (when show? name)))
+
+(defn create-stateful-nullable [name toggle-ref]
+  (let [show? (r/atom true)]
+    (reset! toggle-ref #(swap! show? not))
+    (fn []
+      (when @show?
+        [:div name]))))
+
 (describe "null placeholders"
   (fn []
     (it "should treat undefined as a hole"
@@ -31,4 +42,90 @@
           (th/assert-equal (.-innerHTML container) "<div><div>Hello</div></div>")
 
           ;; root + "Hello" div
-          (th/assert-equal (.-length (.querySelectorAll container "div")) 2))))))
+          (th/assert-equal (.-length (.querySelectorAll container "div")) 2))))
+
+    (it "should efficiently replace null placeholders in parent rerenders (#2350)"
+      (fn []
+        (let [nullable-1 (create-nullable "Nullable 1")
+              nullable-2 (create-nullable "Nullable 2")
+              show? (r/atom false)
+              app (fn []
+                    [:div
+                     [:div (str @show?)]
+                     [nullable-1 {:show? @show?}]
+                     [:div "the middle"]
+                     [nullable-2 {:show? @show?}]])
+              container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+
+          (r/render [app] container)
+          (th/assert-equal (.-innerHTML container)
+                           "<div><div>false</div><div>the middle</div></div>")
+
+          (reset! show? true)
+          (th/assert-equal (.-innerHTML container)
+                           "<div><div>true</div>Nullable 1<div>the middle</div>Nullable 2</div>")
+
+          (reset! show? false)
+          (th/assert-equal (.-innerHTML container)
+                           "<div><div>false</div><div>the middle</div></div>"))))
+
+    (it "should efficiently replace self-updating null placeholders"
+      (fn []
+        (let [toggle-1 (atom nil)
+              toggle-2 (atom nil)
+              nullable-1 (create-stateful-nullable "Nullable" toggle-1)
+              nullable-2 (create-stateful-nullable "Nullable2" toggle-2)
+              app (fn []
+                    [:div
+                     [:div "1"]
+                     [nullable-1]
+                     [:div "3"]
+                     [nullable-2]])
+              container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+
+          (r/render [app] container)
+          (th/assert-equal (.-innerHTML container)
+                           "<div><div>1</div><div>Nullable</div><div>3</div><div>Nullable2</div></div>")
+
+          (@toggle-2)
+          (@toggle-1)
+          (th/assert-equal (.-innerHTML container)
+                           "<div><div>1</div><div>3</div></div>")
+
+          (@toggle-2)
+          (@toggle-1)
+          (th/assert-equal (.-innerHTML container)
+                           "<div><div>1</div><div>Nullable</div><div>3</div><div>Nullable2</div></div>"))))
+
+    (it "should only call unmount once when removing placeholders (#4104)"
+      (fn []
+        (let [ref-calls (atom [])
+              ref-fn (fn [el]
+                       (swap! ref-calls conj (if el :mount :nil)))
+              show? (r/atom true)
+              app (fn []
+                    [:div
+                     [:div "Test2"]
+                     (when @show?
+                       [:div {:ref ref-fn} "Test3"])
+                     [:div "Iframe"]])
+              container (.createElement js/document "div")]
+          (.appendChild js/document.body container)
+
+          (r/render [app] container)
+          (th/assert-equal (.-innerHTML container)
+                           "<div><div>Test2</div><div>Test3</div><div>Iframe</div></div>")
+          (th/assert-equal @ref-calls [:mount])
+
+          (reset! ref-calls [])
+          (reset! show? false)
+
+          (th/assert-equal (.-innerHTML container)
+                           "<div><div>Test2</div><div>Iframe</div></div>")
+          (th/assert-equal @ref-calls [:nil])
+
+          (reset! ref-calls [])
+          (reset! show? false)
+          (th/assert-equal @ref-calls []))))))
