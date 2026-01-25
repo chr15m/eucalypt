93ef8f622882ef63be43a776222731b131efff7e
Chris McCormick <chris@mccormick.cx>
2026-01-25 21:28:42 +0800

    Deprecate helpers/log.

--- src/test/src/helpers.cljs
+++ src/test/src/helpers.cljs
@@ -2,14 +2,14 @@
   (:require ["vitest" :refer [expect]]))
 
 (defn log [& args]
+  ;; DEPRECATED: use js/console.log instead
   (try
     (let [debug-env (.. js/process -env -DEBUG)]
       (when (and (string? debug-env)
                  (.includes debug-env "eucalypt-tests"))
         (.apply (.-log js/console) js/console args)))
-    (catch :default _
-      ;; ignore if process is not available or something else goes wrong
-      )))
+    ;; ignore if process is not available or something else goes wrong
+    (catch :default _)))
 
 (defn assert-equal [actual expected]
   (-> (expect actual) (.toEqual expected)))
